6e8aad4b6a6c4ee37fa023ea994d40aa762b3dec
bgocean <bigocean700@gmail.com>
2024-08-24 20:49:19 +0300

    cancel rebuild

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,6 @@ const fonter = require("gulp-fonter");
 const ttf2woff2 = require("gulp-ttf2woff2");
 const svgSprite = require("gulp-svg-sprite");
 const include = require("gulp-include");
-const replace = require("gulp-replace"); // Новый плагин
 
 function pages() {
   return src("app/pages/*.html")
@@ -25,14 +24,6 @@ function pages() {
     )
     .pipe(dest("app"))
     .pipe(browserSync.stream());
-}
-
-function moveIndex() {
-  return src("app/index.html")
-    .pipe(
-      replace(/src\//g, '') // Обновляем пути к ресурсам
-    )
-    .pipe(dest("./")); // Копируем index.html в корень
 }
 
 function fonts() {
@@ -79,9 +70,8 @@ function sprite() {
 }
 
 function scripts() {
-  return src([
-    "node_modules/swiper/swiper-bundle.js",
-    "app/js/main.js"
+  return src(["node_modules/swiper/swiper-bundle.js",
+   "app/js/main.js"
   ])
     .pipe(concat("main.min.js"))
     .pipe(uglify())
@@ -101,14 +91,14 @@ function styles() {
 function watching() {
   browserSync.init({
     server: {
-      baseDir: "./",
+      baseDir: "app/",
     },
   });
   watch(["app/scss/style.scss", "app/scss/media.scss"], styles);
   watch(["app/images/src"], images);
   watch(["app/js/main.js"], scripts);
-  watch(["app/components/*", "app/pages/*"], series(pages, moveIndex));
-  watch(["index.html"]).on("change", browserSync.reload);
+  watch(["app/components/*", "app/pages/*"], pages);
+  watch(["app/*.html"]).on("change", browserSync.reload);
 }
 
 function cleanDist() {
@@ -125,20 +115,20 @@ function building() {
       "app/images/sprite.svg",
       "app/fonts/*.*",
       "app/js/main.min.js",
-      "index.html", // Учтите, что index.html теперь в корне
+      "app/**/*.html",
     ],
-    { base: "./" }
+    { base: "app" }
   ).pipe(dest("dist"));
 }
 
 exports.styles = styles;
 exports.images = images;
 exports.fonts = fonts;
-exports.pages = series(pages, moveIndex);
+exports.pages = pages;
 exports.building = building;
 exports.sprite = sprite;
 exports.scripts = scripts;
 exports.watching = watching;
 
 exports.build = series(cleanDist, building);
-exports.default = parallel(styles, images, scripts, exports.pages, watching);
+exports.default = parallel(styles, images, scripts, pages, watching);
